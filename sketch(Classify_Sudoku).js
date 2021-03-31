50eeb7f3fb703b0733bd74fe813501f374228528
Markus Schmidgall <markus.schmidgall@googlemail.com>
2021-03-31 15:46:57 +0200

    Add files via upload

--- sketch(Classify_Sudoku).js
+++ sketch(Classify_Sudoku).js
@@ -5,14 +5,21 @@ let resultsDiv;
 const numbers = [];
 const newArr = [];
 let x = 0;
+let video;
+let getImageButton;
 
 function preload(){
-  img = loadImage('Sudoku.jpg');
-  subimg = loadImage('Sudoku.jpg');
+
 }
 
 function setup() {
-  createCanvas(img.width, img.height);
+  video = createCapture(VIDEO);
+  img = video;
+  getImageButton = createButton('Sudoku aufnehmen!');
+  getImageButton.position(100,100);
+  getImageButton.size(300,50);
+  getImageButton.mousePressed(getImage);
+
   let options = {
 	inputs: [28,28,4],
 	task: 'imageClassification',
@@ -24,33 +31,34 @@ function setup() {
     weights: 'model/model.weights.bin'
   }
   numberClassifier.load(modelDetails, modelLoaded);
-  image(img, 0, 0);
-  resultsDiv = createDiv('');
 }
 
 function modelLoaded(){
   console.log('Model loaded!');
 }
 
-function mousePressed(){
-  imgwidth = img.width/9;
-  imgheight = img.height/9;
-  for (let i = 0; i < 9; i++){
-    for (let j = 0; j < 9; j++){
-      subimg = get(j*imgwidth+imgwidth*0.1,i*imgheight+imgheight*0.1,imgwidth*0.8,imgheight*0.8);
-      subimg.resize(28,28);
-      numberClassifier.classify({image: subimg}, gotResults);
-    }
-  }
-  while(numbers.length) newArr.push(numbers.splice(0,9));
-  //resultsDiv.html(newArr.splice(0,9));
-  console.log(newArr);
+function getImage(){
+  console.log(windowWidth, windowHeight);
 }
+// function mousePressed(){
+//   imgwidth = img.width/9;
+//   imgheight = img.height/9;
+//   for (let i = 0; i < 9; i++){
+//     for (let j = 0; j < 9; j++){
+//       subimg = get(j*imgwidth+imgwidth*0.1,i*imgheight+imgheight*0.1,imgwidth*0.8,imgheight*0.8);
+//       subimg.resize(28,28);
+//       numberClassifier.classify({image: subimg}, gotResults);
+//     }
+//   }
+//   while(numbers.length) newArr.push(numbers.splice(0,9));
+//   //resultsDiv.html(newArr.splice(0,9));
+//   console.table(newArr);
+// }
 
 function gotResults(err, results){
   if (err){
     console.error(err);
-    return
+    return;
   }
   let label = results[0].label;
   let confidence = nf(100*results[0].confidence,2,0);
@@ -64,5 +72,5 @@ function gotResults(err, results){
 }
 
 function draw() {
-  image(subimg,0,0);
+  image(img,0,0);
 }
