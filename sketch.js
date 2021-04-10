ba0c6abcdfe6756e84bb88c5dc267515c9141935
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-10 14:25:41 +0200

    Giving grid to solve file

--- sketch.js
+++ sketch.js
@@ -131,9 +131,9 @@ function calculateSudoku() {
     }
 
     console.log(newArr);
-    console.log(getGrid(grid));
+    console.log(getGrid(grid), "Returned Grid");
     newArr = getGrid(grid);
-    console.log(newArr);
+    console.log(newArr, "newArr");
     newArr = grid;
     // while (numbers.length) newArr.push(numbers.splice(0, 9));
     resultsDiv.html(newArr.splice(0, 9));
