226ee5ff019b8c6dbd7f18e0d4d255e960ee1142
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-13 20:01:51 +0200

    refactor when video starts

--- sketch.js
+++ sketch.js
@@ -124,7 +124,7 @@ function videoScreen() {
         video = createCapture(VIDEO);
     }
     video.position(0,0);
-    video.size(windowWidth);
+    video.size(windowWidth, windowWidth);
     _startScreen = false;
     _uploadScreen = false;
     _videoScreen = true;
