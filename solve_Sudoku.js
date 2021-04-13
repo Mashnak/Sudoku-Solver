f3768647e9effe3690e73d97a2635f270c66d219
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-13 18:45:50 +0200

    showing logo

--- solve_Sudoku.js
+++ solve_Sudoku.js
@@ -1,3 +1,15 @@
+// Hardcoded, minimizes need for dynamic code later
+var square_coordinates = [
+    [1, 1, 1, 2, 2, 2, 3, 3, 3],
+    [1, 1, 1, 2, 2, 2, 3, 3, 3],
+    [1, 1, 1, 2, 2, 2, 3, 3, 3],
+    [4, 4, 4, 5, 5, 5, 6, 6, 6],
+    [4, 4, 4, 5, 5, 5, 6, 6, 6],
+    [4, 4, 4, 5, 5, 5, 6, 6, 6],
+    [7, 7, 7, 8, 8, 8, 9, 9, 9],
+    [7, 7, 7, 8, 8, 8, 9, 9, 9],
+    [7, 7, 7, 8, 8, 8, 9, 9, 9]
+]
 
 function get_row(board, row) {
     // Given a board, we can return a single row
