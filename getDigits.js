bb42bb8ef6446fe0cd5f883f4821d9a09e870943
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-14 00:08:32 +0200

    testing numbers

--- getDigits.js
+++ getDigits.js
@@ -1,5 +1,15 @@
 let numbers = [];
-
+const grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [0, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+];
 function getDigits(img) {
     let imgwidth = img.width / 9;
     let imgheight = img.height / 9;
@@ -13,6 +23,7 @@ function getDigits(img) {
     if (numbers.length===81) {
         return numbers;
     }
+    return grid;
 }
 
 
