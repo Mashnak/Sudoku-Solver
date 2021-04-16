16a87a631a2b3fd0685fb5c8cb81f8bd169b9a6c
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-16 21:13:24 +0200

    test getDigits

--- sketch.js
+++ sketch.js
@@ -92,8 +92,32 @@ function uploadScreen() {
     calculateButton.hide();
     resetButton.show();
     /***********************/
-    img = loadImage("Sudoku.jpg");
-    img2 = img;
+    function dateiauswahl(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        var gewaehlteDateien = evt.dataTransfer.files; // FileList Objekt
+
+
+        var output = [];
+        for (var i = 0, f; f = gewaehlteDateien[i]; i++) {
+            output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                f.size, ' bytes, last modified: ',
+                f.lastModified.toLocaleDateString(), '</li>');
+        }
+        document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+    }
+
+    function handleDragOver(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = 'copy';
+    }
+
+    // Initialisiere Drag&Drop EventListener
+    var dropZone = document.getElementById('dropzone');
+    dropZone.addEventListener('dragover', handleDragOver, false);
+    dropZone.addEventListener('drop', dateiauswahl, false);
 }
 
 /**
