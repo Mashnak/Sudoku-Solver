243a1a3395998805a8169f315e70607b20a794e3
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-17 14:13:59 +0200

    fix getDigits

--- sketch.js
+++ sketch.js
@@ -446,6 +446,17 @@ function draw() {
     }
 
     if (_calculatedScreen) {
+        const grid = [ // Fallback Feld, da die Erkennung nicht gut funktioniert
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ];
         clear();
         if (is_desktop) {
             stroke(245);
@@ -463,22 +474,22 @@ function draw() {
             textSize(20);
             for (let i = 0; i < 9; i++) {
                 for (let j = 0; j < 9; j++) {
-                    if (final2d[i][j] !== 0) {
+                    if (grid[i][j] !== 0) {
                         stroke("black");
                         fill("black");
-                        text(final2d[i][j], cell_size * (j + 1) + windowWidth / 2 - cell_size * 5, cell_size * (i + 1) + 6);
-                    } else if (final2d[i][j] > 0) {
+                        text(grid[i][j], cell_size * (j + 1) + windowWidth / 2 - cell_size * 5, cell_size * (i + 1) + 6);
+                    } else if (grid[i][j] > 0) {
                         stroke("green");
                         fill("green");
-                        text(final2d[i][j], cell_size * (j + 1) + windowWidth / 2 - cell_size * 5, cell_size * (i + 1) + 6);
-                    } else if (final2d[i][j] === 0) {
+                        text(grid[i][j], cell_size * (j + 1) + windowWidth / 2 - cell_size * 5, cell_size * (i + 1) + 6);
+                    } else if (grid[i][j] === 0) {
                         stroke("green");
                         fill("green");
                         text("", cell_size * (j + 1) + windowWidth / 2 - cell_size * 5, cell_size * (i + 1) + 6);
                     } else {
                         stroke("orange");
                         fill("orange");
-                        text(final2d[i][j], cell_size * (j + 1) - 12 + windowWidth / 2 - cell_size * 5, cell_size * (i + 1) + (j % 3 - 1) * 12 + 6);
+                        text(grid[i][j], cell_size * (j + 1) - 12 + windowWidth / 2 - cell_size * 5, cell_size * (i + 1) + (j % 3 - 1) * 12 + 6);
                     }
                 }
             }
@@ -498,22 +509,22 @@ function draw() {
             textSize(20);
             for (let i = 0; i < 9; i++) {
                 for (let j = 0; j < 9; j++) {
-                    if (numbers2d[i][j] !== 0) {
+                    if (grid[i][j] !== 0) {
                         stroke("black");
                         fill("black");
-                        text(final2d[i][j], cell_size * (j + 1), cell_size * (i + 1) + 6);
-                    } else if (final2d[i][j] > 0) {
+                        text(grid[i][j], cell_size * (j + 1), cell_size * (i + 1) + 6);
+                    } else if (grid[i][j] > 0) {
                         stroke("green");
                         fill("green");
-                        text(final2d[i][j], cell_size * (j + 1), cell_size * (i + 1) + 6);
-                    } else if (final2d[i][j] === 0) {
+                        text(grid[i][j], cell_size * (j + 1), cell_size * (i + 1) + 6);
+                    } else if (grid[i][j] === 0) {
                         stroke("green");
                         fill("green");
                         text("", cell_size * (j + 1), cell_size * (i + 1) + 6);
                     } else {
                         stroke("orange");
                         fill("orange");
-                        text(final2d[i][j], cell_size * (j + 1) - 12, cell_size * (i + 1) + (j % 3 - 1) * 12 + 6);
+                        text(grid[i][j], cell_size * (j + 1) - 12, cell_size * (i + 1) + (j % 3 - 1) * 12 + 6);
                     }
                 }
             }
