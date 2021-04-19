d891601fda8415a7e7ddca271924e0cafa38466e
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-19 12:45:44 +0200

    image(subimages)

--- sketch.js
+++ sketch.js
@@ -236,6 +236,17 @@ async function sudokuScreen() {
  */
 function calculatedScreen() {
     console.log("CalculatedScreen");
+    const grid = [ // Fallback Feld, da die Erkennung nicht gut funktioniert
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ];
     _startScreen = false;
     _uploadScreen = false;
     _videoScreen = false;
