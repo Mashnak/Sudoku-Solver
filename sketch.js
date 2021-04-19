2844938ec1f62b8ee5293398c0b2a7f5f047523e
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-19 12:42:42 +0200

    image(subimages)

--- sketch.js
+++ sketch.js
@@ -148,16 +148,6 @@ function videoScreen() {
  *
  */
 async function uploadImageScreen() {
-    const grid = [ // Fallback Feld, da die Erkennung nicht gut funktioniert
-        [5, 3, 0, 0, 7, 0, 0, 0, 0],
-        [6, 0, 0, 1, 9, 5, 0, 0, 0],
-        [0, 9, 8, 0, 0, 0, 0, 6, 0],
-        [8, 0, 0, 0, 6, 0, 0, 0, 3],
-        [4, 0, 0, 8, 0, 3, 0, 0, 1],
-        [7, 0, 0, 0, 2, 0, 0, 0, 6],
-        [0, 6, 0, 0, 0, 0, 2, 8, 0],
-        [0, 0, 0, 4, 1, 9, 0, 0, 5],
-        [0, 0, 0, 0, 8, 0, 0, 7, 9]
     console.log("UploadImageScreen");
     _startScreen = false;
     _uploadScreen = false;
