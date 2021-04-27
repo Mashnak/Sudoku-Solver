fe1dec8adf37c458b4f2c3dc688d378e327d9d4d
Schmiddi2405 <markus.schmidgall@googlemail.com>
2021-04-27 12:22:12 +0200

    rename variables

--- solve_Sudoku.js
+++ solve_Sudoku.js
@@ -13,41 +13,38 @@ var square_coordinates = [
 
 function getGrid(grid) {
     let newArr = [];
-
-
-    for(let i = 0; i < solve_sudoku(grid).length; i++)
-    {
+    for (let i = 0; i < solve_sudoku(grid).length; i++) {
         newArr = newArr.concat(solve_sudoku(grid)[i]);
     }
     return newArr;
 }
 
-function get_row(board, row) {
-    return board[row]
+function get_row(grid, row) {
+    return grid[row]
 }
 
-function get_column(board, column) {
+function get_column(grid, column) {
     var col = []
     for (let row = 0; row < 9; row++) {
-        col.push(board[row][column]);
+        col.push(grid[row][column]);
     }
     return col
 }
 
-function get_square(board, square) {
+function get_square(grid, square) {
     let cells = []
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
             if (square === square_coordinates[r][c]) {
-                cells.push(board[r][c])
+                cells.push(grid[r][c])
             }
         }
     }
     return cells
 }
 
-function couting_method(board, r, c) {
-    let used = [...get_row(board, r), ...get_column(board, c), ...get_square(board, square_coordinates[r][c])]
+function counting_method(grid, r, c) {
+    let used = [...get_row(grid, r), ...get_column(grid, c), ...get_square(grid, square_coordinates[r][c])]
     let possibilities = []
     for (let p = 1; p <= 9; p++) {
         if (!used.includes(p)) {
@@ -55,15 +52,15 @@ function couting_method(board, r, c) {
         }
     }
     if (possibilities.length === 1) {
-        board[r][c] = possibilities[0]
+        grid[r][c] = possibilities[0]
         return true
     } else {
-        board[r][c] = possibilities
+        grid[r][c] = possibilities
         return false
     }
 }
 
-function cross_hatching(board, possibilities, segment, r, c) {
+function cross_hatching(grid, possibilities, segment, r, c) {
     let updated = false
     for (i = 0; i < possibilities.length; i++) {
         let possibility = possibilities[i]
@@ -80,7 +77,7 @@ function cross_hatching(board, possibilities, segment, r, c) {
             }
         })
         if (counter === 1) {
-            board[r][c] = possibility
+            grid[r][c] = possibility
             updated = true
             break
         }
@@ -91,49 +88,47 @@ function cross_hatching(board, possibilities, segment, r, c) {
 function compare(expected, actual) {
     let array1 = expected.slice()
     let array2 = actual.slice()
-    return array1.length === array2.length && array1.sort().every(function (value, index) { return value === array2.sort()[index] });
+    return array1.length === array2.length && array1.sort().every(function (value, index) {
+        return value === array2.sort()[index]
+    });
 }
 
-function is_solved(board) {
+function is_solved(grid) {
     let expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     let valid = true
     for (r = 0; r < 9 && valid === true; r++) {
-        if (!compare(expected, get_row(board, r))) {
+        if (!compare(expected, get_row(grid, r))) {
             valid = false
         }
     }
     for (c = 0; c < 9 && valid === true; c++) {
-        if (!compare(expected, get_column(board, c))) {
+        if (!compare(expected, get_column(grid, c))) {
             valid = false
         }
     }
     for (q = 1; q < 9 && valid === true; q++) {
-        if (!compare(expected, get_square(board, q))) {
+        if (!compare(expected, get_square(grid, q))) {
             valid = false
         }
     }
     return valid
 }
 
-function solve_backtracking(orig_board) {
-    let board = JSON.parse(JSON.stringify(orig_board));
-    let completed_board;
+function solve_backtracking(orig_grid) {
+    let grid = JSON.parse(JSON.stringify(orig_grid));
+    let completed_grid;
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
-            if (board[r][c] === 0) {
-                couting_method(board, r, c)
-                if (is_solved(board)) return board;
-                let cell = board[r][c]
-                // If we just created a list of possibilities, iterate them and recurse
+            if (grid[r][c] === 0) {
+                counting_method(grid, r, c)
+                if (is_solved(grid)) return grid;
+                let cell = grid[r][c]
                 if (Array.isArray(cell)) {
                     for (let i = 0; i < cell.length; i++) {
-                        // Create a temporary board for each recursion.
-                        let board_2 = JSON.parse(JSON.stringify(board));
-                        // Choose a value
-                        board_2[r][c] = cell[i]
-                        // Recurse again using new board
-                        if (completed_board = solve_backtracking(board_2)) {
-                            return completed_board;
+                        let grid_2 = JSON.parse(JSON.stringify(grid));
+                        grid_2[r][c] = cell[i]
+                        if (completed_grid = solve_backtracking(grid_2)) {
+                            return completed_grid;
                         }
                     }
                     return false // dead end
@@ -146,48 +141,48 @@ function solve_backtracking(orig_board) {
 
 }
 
-function naked_single(board) {
+function naked_single(grid) {
     updated = false
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
-            if (board[r][c] === 0) {
-                updated = couting_method(board, r, c) || updated
+            if (grid[r][c] === 0) {
+                updated = counting_method(grid, r, c) || updated
             }
         }
     }
 
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
-            if (Array.isArray(board[r][c])) {
-                let possibilities = board[r][c]
-                updated = cross_hatching(board, possibilities, get_row(board, r), r, c) ||
-                    cross_hatching(board, possibilities, get_column(board, c), r, c) ||
-                    cross_hatching(board, possibilities, get_square(board, square_coordinates[r][c]), r, c) || updated
+            if (Array.isArray(grid[r][c])) {
+                let possibilities = grid[r][c]
+                updated = cross_hatching(grid, possibilities, get_row(grid, r), r, c) ||
+                    cross_hatching(grid, possibilities, get_column(grid, c), r, c) ||
+                    cross_hatching(grid, possibilities, get_square(grid, square_coordinates[r][c]), r, c) || updated
             }
         }
     }
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
-            if (Array.isArray(board[r][c])) {
-                board[r][c] = 0
+            if (Array.isArray(grid[r][c])) {
+                grid[r][c] = 0
             }
         }
     }
     return updated
 }
 
-function solve_sudoku(board) {
+function solve_sudoku(grid) {
     let updated = true, solved = false
     while (updated && !solved) {
-        updated = naked_single(board)
-        solved = is_solved(board)
+        updated = naked_single(grid)
+        solved = is_solved(grid)
     }
     if (!solved) {
-        board = solve_backtracking(board)
-        solved = is_solved(board)
+        grid = solve_backtracking(grid)
+        solved = is_solved(grid)
     }
 
-    return board
+    return grid
 }
 
 
